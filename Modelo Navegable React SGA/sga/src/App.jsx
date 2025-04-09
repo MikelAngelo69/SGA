c9b09c61cb2487c6f1c0bb6af94814b893fb44de
mikel <miguelitomendez454@gmail.com>
2025-04-08 23:17:46 -0500

    change if in carousel

---
+++ Modelo Navegable React SGA/sga/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/sign-in/sign-in.component";
+import FrontPage from "./pages/front-page/front_page.page";
+import HomeSeller from "./pages/home_seller/home_seller.page";
+import CreateUser from "./pages/home_seller/create_user/create_user.component";
+
+function App() {
+  return (
+
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/HomeSeller" element={<HomeSeller />} />
+        <Route path="/HomeSeller/CreateUser" element={<CreateUser />} />
+      </Routes>
+
+  );
+}
+
+export default App;
