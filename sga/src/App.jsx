1f3ee19a76fc6f16bdc813425864586e57205093
laura <ortizizquierdolaurakaria@gmail.com>
2025-04-09 20:51:14 -0500

    contact form

--- sga/src/App.jsx
+++ sga/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/sign-in/sign-in.component";
 import FrontPage from "./pages/front-page/front_page.page";
 import HomeSeller from "./pages/home_seller/home_seller.page";
 import CreateUser from "./pages/home_seller/create_user/create_user.component";
+import Contact from "./pages/contact/contact.component";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<FrontPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/contacto" element={<Contact />} />
         <Route path="/HomeSeller" element={<HomeSeller />} />
         <Route path="/HomeSeller/CreateUser" element={<CreateUser />} />
       </Routes>
